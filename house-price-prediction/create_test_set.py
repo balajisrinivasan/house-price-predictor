9eef9278b287039f19b9a7ba175372a340e25e6d
Balaji Srinivasan <balaji.srini86@gmail.com>
2025-12-25 22:36:27 -0800

    Explore training set creation strategies

--- house-price-prediction/create_test_set.py
+++ house-price-prediction/create_test_set.py
@@ -1,6 +1,9 @@
 from pathlib import Path
+from zlib import crc32
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 def shuffle_and_split_data(data, test_ratio, rng):
     shuffled_indices = rng.permutation(len(data))
@@ -9,12 +12,58 @@ def shuffle_and_split_data(data, test_ratio, rng):
     train_indices = shuffled_indices[test_set_size:]
     return data.iloc[train_indices], data.iloc[test_indices]
 
+def is_id_in_test_set(identifier, test_ratio):
+    return crc32(np.int64(identifier)) < test_ratio * 2 ** 32;
+
+def split_data_with_id_hash(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: is_id_in_test_set(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
 def main():
     housing_full = pd.read_csv(Path("datasets/housing/housing.csv"))
-    rng = np.random.default_rng()
-    train_set, test_set = shuffle_and_split_data(housing_full, 0.2, rng)
+    housing_with_id = housing_full.reset_index()
+    housing_with_id["id"] = (housing_full["longitude"] * 1000 + housing_full["latitude"])
+
+    #rng = np.random.default_rng()
+    train_set, test_set = split_data_with_id_hash(housing_with_id, 0.2, "id")
     print(len(train_set))
     print(len(test_set))
+
+    # similar to random sampling, uses a seed as starting point
+    train_set, test_set = train_test_split(housing_full, test_size=0.2, random_state=42)
+    print(len(train_set))
+    print(len(test_set))
+    print(test_set["total_bedrooms"].isnull().sum())
+
+    # stratified sampling based on income category
+    housing_full["income_cat"] = pd.cut(
+        housing_full["median_income"],
+        bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+        labels=[1,2,3,4,5]
+    )
+    # cat_counts = housing_full["income_cat"].value_counts().sort_index();
+    # cat_counts.plot.bar(rot=0, grid=True)
+    # plt.xlabel("Income categroy")
+    # plt.ylabel("Number of districts")
+    # plt.show()
+
+    # strat_train_set, strat_test_set = train_test_split(
+    #     housing_full, test_size=0.2, stratify=housing_full["income_cat"], random_state=42
+    # )
+    # print(strat_test_set["income_cat"].value_counts()/len(strat_test_set))
+
+    # for set__ in (strat_train_set, strat_test_set):
+    #     set__.drop("income_cat", axis=1, inplace=True)
+
+    np.random.seed(52)
+    print(np.random.rand(5))
+
+    np.random.seed(52)
+    print(np.random.rand(5))
+
+    np.random.seed(52)
+    print(np.random.rand(5))
 
 if __name__ == "__main__":
     main()
